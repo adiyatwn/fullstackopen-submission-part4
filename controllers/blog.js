897d35e14b050ab93fd027ab88780f43d697c327
Adiyat Wira Negara <kazutonine@gmail.com>
2024-10-15 11:20:51 +0700

    Finished exercise 4.8

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,22 +1,17 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  const result = await blog.save()
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  response.status(201).json(result)
 })
 
 module.exports = blogRouter
